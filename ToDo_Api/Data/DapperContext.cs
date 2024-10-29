271f919acc049839cab3a4c5d74d0876a01369e6
PaballoMmamaro <paballoelisa22@gmail.com>
2024-10-29 10:25:41 +0200

    DapperContext done

---
+++ ToDo_Api/Data/DapperContext.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ToDo_Api.DapperContext
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<DapperContext> _logger;
+        private string _connectionString;
+
+        public DapperContext(IConfiguration config, ILogger<DapperContext> logger)
+        {
+            _config = config;
+            _logger = logger;
+            _connectionString = _config.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<T>> QueryMultipleRows<T>(string query)
+        {
+            try
+            {
+                using IDbConnection connection = new SqlConnection(_connectionString);
+
+                var data =  await connection.QueryAsync<T>(query);
+
+                return data.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in the DAPPER CONTEXT while trying to query multiple rows: {ex}", ex.Message);
+                return default;
+            }
+        }
+
+        public async Task<List<T>> QueryMultipleRowsWithParams<T>(string query, DynamicParameters parameters)
+        {
+            try
+            {
+                using IDbConnection connection = new SqlConnection(_connectionString);
+
+                var data = await connection.QueryAsync<T>(query, parameters);
+
+                return data.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in the DAPPER CONTEXT while trying to query multiple rows with parameters: {ex}", ex.Message);
+                return default;
+            }
+        }
+
+        public async Task<T> QuerySingleRow<T>(string query, DynamicParameters parameters)
+        {
+            try
+            {
+                using IDbConnection connection = new SqlConnection(_connectionString);
+
+                var data = await connection.QueryFirstOrDefaultAsync<T>(query, parameters);
+
+                return data;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in the DAPPER CONTEXT while trying to query a single row: {ex}", ex.Message);
+                return default;
+            }
+        }
+
+        public async Task<bool> ExecuteQuery(string query, DynamicParameters parameters)
+        {
+            try
+            {
+                using IDbConnection connection = new SqlConnection(_connectionString);
+
+                var rowsAffected = await connection.ExecuteAsync(query,parameters);
+
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in the DAPPER CONTEXT while trying to execute a query: {ex}", ex.Message);
+                return default;
+            }
+        }
+
+    }
+}
--- ToDo_Api/Program.cs
+++ ToDo_Api/Program.cs
@@ -1,3 +1,5 @@
+using ToDo_Api.DapperContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registering my services
+builder.Services.AddSingleton<DapperContext>();
 
 var app = builder.Build();
 
